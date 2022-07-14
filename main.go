4507a19c04566c5ddcabbc153f8b8271bc16d0df
KarbozovDamir <damirkap89@mail.ru>
2022-07-14 17:33:09 +0600

    feat: add: functions

--- main.go
+++ main.go
@@ -5,7 +5,37 @@ import (
 	"os"
 )
 
+type Arguments map[string]string
+
+type User struct {
+	Id    string `json:"id"`
+	Email string `json:"email"`
+	Age   int    `json:"age"`
+}
+
+typeOfOperation:= map[string]bool{
+	"add":      true,
+	"list":     true,
+	"findById": true,
+	"remove":   true,
+}
+
 func Perform(args Arguments, writer io.Writer) error {
+	if len(typeOfOperation) == 0 {
+		return errors.New("-operation flag has to be specified")
+	}
+}
+
+func add(filename string, item string) ([]byte, error) {
+}
+
+func findById(fileContents []byte, writer io.Writer, args Arguments) error {
+}
+
+func list(fileContents []byte, writer io.Writer) error {
+}
+
+func remove(fileContents []byte, writer io.Writer, args Arguments) error {
 }
 
 func main() {
